09fec2edc644ddeee70ca151e81476c3437ac321
arozovyk <rozovyk.artem@gmail.com>
2021-11-18 09:13:17 +0000

    Deploying to gh-pages from  @ 67072713f3fe182b2421d1f0b3a2dc1bda5635f3 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (8b50ca9) (2021/11/14 18:33)',
+    VERSION: 'master (6707271) (2021/11/18 09:13)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
